dac7615844af86ec1d6098435e80e6000bac8853
“Nicole <itsnicoleduffy@gmail.com>
2023-11-01 16:38:56 +0100

    submit form done

--- src/pages/SongsPage.jsx
+++ src/pages/SongsPage.jsx
@@ -3,46 +3,46 @@ import { Link } from "react-router-dom";
 import "../styles/SongsPage.css";
 
 function SongsPage() {
-    const [songs, setSongs] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [songs, setSongs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const fetchAllSongs = async () => {
-        try {
-            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/songs`);
+  const fetchAllSongs = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/songs`);
 
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
 
-            const songsData = await response.json();
-            setSongs(songsData);
-            setIsLoading(false);
-        } catch (error) {
-            console.error("There's been an error fetching songs", error);
-            setIsLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchAllSongs();
-    }, []);
-
-    if (isLoading) {
-        return <div>Loading...</div>;
+      const songsData = await response.json();
+      setSongs(songsData);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("There's been an error fetching songs", error);
+      setIsLoading(false);
     }
+  };
 
-    return (
-        <div className="songs-grid-container">
-            <Link to="/uploadSong">Upload Your Release</Link>
-            {songs.map((song) => (
-                <div key={song._id} className="all-songs">
-                    <Link to={`/songs/${song._id}`}>
-                        <h2 className="song-title">{song.artist} - {song.title} /</h2>
-                    </Link>
-                </div>
-            ))}
-        </div>
-    );
+  useEffect(() => {
+    fetchAllSongs();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="songs-grid-container">
+        <Link to="/uploadSong">Upload Your Release</Link>
+        {songs.map((song) => (
+            <div key={song._id} className="all-songs">
+                <Link to={`/songs/${song._id}`}>
+                    <h2 className="song-title">{song.artist} - {song.title} /</h2>
+                </Link>
+            </div>
+        ))}
+    </div>
+);
 }
 
 export default SongsPage;
--- src/pages/UploadSong.jsx
+++ src/pages/UploadSong.jsx
@@ -1,100 +1,80 @@
-import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "../styles/UploadSong.css";
 
 function UploadSong() {
-    const navigate = useNavigate();
-    const [title, setTitle] = useState("");
-    const [artist, setArtist] = useState("");
-    const [album, setAlbum] = useState("");
-    const [genre, setGenre] = useState("");
-    const [label, setLabel] = useState("");
-    const [released, setReleased] = useState("");
-    const [image, setImage] = useState("");
-    const [error, setError] = useState("");
+  const [title, setTitle] = useState("");
+  const [artist, setArtist] = useState("");
+  const [album, setAlbum] = useState("");
+  const [genre, setGenre] = useState("");
+  const [label, setLabel] = useState("");
+  const [released, setReleased] = useState("");
+  const [image, setImage] = useState("");
+  const [error, setError] = useState("");
 
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        setError("");
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-        const values = {
-            title,
-            artist,
-            album,
-            genre,
-            label, 
-            released,
-            image,
-        };
+    setTitle("");
+    setArtist("");
+    setAlbum("");
+    setGenre("");
+    setLabel("");
+    setReleased("");
+    setImage("");
+  };
 
-        try {
-            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/songs`, {
-                method: 'POST',
-                body: JSON.stringify(values),
-                headers: { 'Content-Type': 'application/json' },
-            });
-
-            if (response.ok) {
-                const newSong = await response.json();
-                navigate(`/songs/${newSong._id}`); 
-            } else {
-                setError("An error occurred");
-            }
-        } catch (error) {
-            setError("An error occurred");
-        }
-    }
-
-    return (
-        <div>
-            <h1>Upload a New Song</h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    type="text"
-                    placeholder="Title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Artist"
-                    value={artist}
-                    onChange={(e) => setArtist(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Album"
-                    value={album}
-                    onChange={(e) => setAlbum(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Genre"
-                    value={genre}
-                    onChange={(e) => setGenre(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Label"
-                    value={label}
-                    onChange={(e) => setLabel(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Release Date"
-                    value={released}
-                    onChange={(e) => setReleased(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Image URL"
-                    value={image}
-                    onChange={(e) => setImage(e.target.value)}
-                />
-                <button type="submit">Upload</button>
-            </form>
-            {error && <p>{error}</p>}
-        </div>
-    );
+  return (
+    <div className="upload-song-container">
+      <form className="form" onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Artist"
+          value={artist}
+          onChange={(e) => setArtist(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Album"
+          value={album}
+          onChange={(e) => setAlbum(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Genre"
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Label"
+          value={label}
+          onChange={(e) => setLabel(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Release Date"
+          value={released}
+          onChange={(e) => setReleased(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Image URL"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+        <button type="submit" className="upload-button">
+          Upload
+        </button>
+      </form>
+      {error && <p className="error-message">{error}</p>}
+    </div>
+  );
 }
 
 export default UploadSong;
