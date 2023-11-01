ec6ec07d4ad7b49c98512a497215d89c25657d28
“Nicole <itsnicoleduffy@gmail.com>
2023-11-01 13:20:53 +0100

    song details done for now

--- src/pages/SongDetailsPage.jsx
+++ src/pages/SongDetailsPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
-import '../styles/SongsDetails.css'
+import "../styles/SongsDetails.css";
 
 function SongDetailsPage() {
   const { _id } = useParams();
@@ -48,13 +48,18 @@ function SongDetailsPage() {
   };
 
   const handleAddToFavorites = () => {
-    const songId = song._id; 
+    const songId = song._id;
     const userId = user.userId;
-    console.log(user)
+    console.log(user);
 
-    fetch(`${import.meta.env.VITE_API_URL}/api/users/add-to-favourites/${userId}/${songId}`, {
-      method: "POST",
-    })
+    fetch(
+      `${
+        import.meta.env.VITE_API_URL
+      }/api/users/add-to-favourites/${userId}/${songId}`,
+      {
+        method: "POST",
+      }
+    )
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -70,17 +75,26 @@ function SongDetailsPage() {
   }
 
   return (
-    <>
-      <img src={song.image} alt={song.title} className="songImage" />
-      <h1>{song.title}</h1>
-      <h1>{song.artist}</h1>
-      <p>{song.album}</p>
-      <p>{song.genre}</p>
-      <p>{song.label}</p>
-      <p>{song.released}</p>
-      <button className="delete-button" onClick={handleDelete}>Delete Song</button>
-      <button className="favourites-button" onClick={handleAddToFavorites}>Add to Favorites</button>
-    </>
+    <div className="song-details-container">
+      <div className="image-container">
+        <img src={song.image} alt={song.title} className="songImage" />
+      </div>
+      <div className="info-container">
+        <p className="songTitle">  
+          {song.artist} - {song.title}
+        </p>
+        <p>{song.album}</p>
+        <p>{song.genre}</p>
+        <p>{song.label}</p>
+        <p>{song.released}</p>
+        <button className="delete-button" onClick={handleDelete}>
+          Delete Song
+        </button>
+        <button className="favourites-button" onClick={handleAddToFavorites}>
+          Add to Favorites
+        </button>
+      </div>
+    </div>
   );
 }
 
