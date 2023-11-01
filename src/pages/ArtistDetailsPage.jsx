635dd270e13205a2d60f372ceffdc29f13b0c71e
“Nicole <itsnicoleduffy@gmail.com>
2023-11-01 14:18:08 +0100

    ready to deploy

--- src/pages/ArtistDetailsPage.jsx
+++ src/pages/ArtistDetailsPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
+import '../styles/ArtistDetails.css'
 
 function ArtistDetailsPage() {
   const { _id } = useParams();
@@ -9,14 +10,13 @@ function ArtistDetailsPage() {
   const [artist, setArtist] = useState(null);
   const apiUrl = `${import.meta.env.VITE_API_URL}/api/artists/${_id}`;
   const navigate = useNavigate();
-  
 
   useEffect(() => {
     if (_id) {
       fetch(apiUrl)
         .then((response) => {
           if (!response.ok) {
-            throw new Error("Network response was not ok");
+            throw new new Error("Network response was not ok");
           }
           return response.json();
         })
@@ -28,10 +28,10 @@ function ArtistDetailsPage() {
         });
     }
   }, [_id]);
+
   const handleAddToFavorites = () => {
-    const artistId = artist._id; 
+    const artistId = artist._id;
     const userId = user.userId;
-    console.log(user)
 
     fetch(`${import.meta.env.VITE_API_URL}/api/users/add-artist-to-favourites/${userId}/${artistId}`, {
       method: "POST",
@@ -51,13 +51,17 @@ function ArtistDetailsPage() {
   }
 
   return (
-    <>
-      <img src={artist.image} alt={artist.name} className="artist" />
-      <h1>{artist.name}</h1>
-      <p>{artist.realName}</p>
-      <p>{artist.location}</p>
-      <button onClick={handleAddToFavorites}>Add to Favorites</button>
-    </>
+    <div className="artist-details-container">
+      <div className="artist-image-container">
+        <img src={artist.image} alt={artist.name} className="artistImage" />
+      </div>
+      <div className="artist-details">
+        <p className="artist-font">{artist.name}</p>
+        <p>{artist.realName}</p>
+        <p>{artist.location}</p>
+        <button className="artist-favourites-button" onClick={handleAddToFavorites}>Add to Favorites</button>
+      </div>
+    </div>
   );
 }
 
