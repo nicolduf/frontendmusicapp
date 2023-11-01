fc5fedeb2fed44453ce8cc2bad44c85a704523da
“Nicole <itsnicoleduffy@gmail.com>
2023-11-01 08:36:09 +0100

    SHE HAS A MVP

--- src/pages/ArtistDetailsPage.jsx
+++ src/pages/ArtistDetailsPage.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
 
 function ArtistDetailsPage() {
   const { _id } = useParams();
+  const { user } = useContext(AuthContext);
   const [artist, setArtist] = useState(null);
   const apiUrl = `${import.meta.env.VITE_API_URL}/api/artists/${_id}`;
   const navigate = useNavigate();
@@ -26,24 +28,23 @@ function ArtistDetailsPage() {
         });
     }
   }, [_id]);
+  const handleAddToFavorites = () => {
+    const artistId = artist._id; 
+    const userId = user.userId;
+    console.log(user)
 
-  // const addToFavorites = () => {
-  //   fetch(`${import.meta.env.VITE_API_URL}/favorites`, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ artistId: artistId }),
-  //   })
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error("Network response was not ok");
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error adding artist to favorites:", error);
-  //     });
-  // };
+    fetch(`${import.meta.env.VITE_API_URL}/api/users/add-artist-to-favourites/${userId}/${artistId}`, {
+      method: "POST",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+      })
+      .catch((error) => {
+        console.error("Error adding artist to favorites", error);
+      });
+  };
 
   if (!artist) {
     return <div>Loading...</div>;
@@ -55,7 +56,7 @@ function ArtistDetailsPage() {
       <h1>{artist.name}</h1>
       <p>{artist.realName}</p>
       <p>{artist.location}</p>
-      {/* <button onClick={addToFavorites}>Add to Favorites</button> */}
+      <button onClick={handleAddToFavorites}>Add to Favorites</button>
     </>
   );
 }
