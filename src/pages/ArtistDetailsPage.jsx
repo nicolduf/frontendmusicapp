9d624b1c21ca3503ef19217a6b411df92056210b
“Nicole <itsnicoleduffy@gmail.com>
2023-11-30 13:53:41 +0100

    added remove artist fav button

--- src/pages/ArtistDetailsPage.jsx
+++ src/pages/ArtistDetailsPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
-import '../styles/ArtistDetails.css';
+import "../styles/ArtistDetails.css";
 
 function ArtistDetailsPage() {
   const { _id } = useParams();
@@ -16,7 +16,7 @@ function ArtistDetailsPage() {
       fetch(apiUrl)
         .then((response) => {
           if (!response.ok) {
-            throw new new Error("Network response was not ok");
+            throw new Error("Network response was not ok");
           }
           return response.json();
         })
@@ -33,16 +33,57 @@ function ArtistDetailsPage() {
     const artistId = artist._id;
     const userId = user.userId;
 
-    fetch(`${import.meta.env.VITE_API_URL}/api/users/add-artist-to-favourites/${userId}/${artistId}`, {
-      method: "POST",
-    })
+    fetch(
+      `${
+        import.meta.env.VITE_API_URL
+      }/api/users/add-artist-to-favourites/${userId}/${artistId}`,
+      {
+        method: "POST",
+      }
+    )
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
+        // Optionally, you can update the local state to reflect the addition
+        setArtist((prevArtist) => ({
+          ...prevArtist,
+          // Adjust your data structure as needed
+          // For example, if you have a 'isFavorite' property, set it to true
+          isFavorite: true,
+        }));
       })
       .catch((error) => {
         console.error("Error adding artist to favorites", error);
+      });
+  };
+
+  const handleRemoveFromFavorites = () => {
+    const artistId = artist._id;
+    const userId = user.userId;
+
+    fetch(
+      `${
+        import.meta.env.VITE_API_URL
+      }/api/users/remove-artist-from-favourites/${userId}/${artistId}`,
+      {
+        method: "DELETE", // Assuming you have an endpoint to handle removal using the DELETE method
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        // Optionally, you can update the local state to reflect the removal
+        setArtist((prevArtist) => ({
+          ...prevArtist,
+          // Adjust your data structure as needed
+          // For example, if you have a 'isFavorite' property, set it to false
+          isFavorite: false,
+        }));
+      })
+      .catch((error) => {
+        console.error("Error removing artist from favorites", error);
       });
   };
 
@@ -56,9 +97,16 @@ function ArtistDetailsPage() {
         <p className="artist">{artist.name}</p>
         <p className="artist-realName">{artist.realName}</p>
         <p className="artist-location">{artist.location}</p>
-        <button className="artist-favourites-button" onClick={handleAddToFavorites}>Add to Favorites</button>
+        <div>
+          <button className="artist-favourites-button" onClick={handleAddToFavorites}>
+            Add to Favorites
+          </button>
+          <button className="artist-remove-button" onClick={handleRemoveFromFavorites}>
+            Remove from Favorites
+          </button>
         </div>
-        <img src={artist.image} alt={artist.name} className="artist-image" />
+      </div>
+      <img src={artist.image} alt={artist.name} className="artist-image" />
     </div>
   );
 }
